a3619adc9b9ae132f22358f8a199e7d107dd61d6
your_github_username <talassiofilo@gmail.com>
2022-05-10 11:59:15 +0200

    2nd mod

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
+		echo date("g:i A l, F j Y.");
+		print "Hello, World! php version is " . PHP_VERSION . "\n";
+			?>
+			</p>
        <h2>DZJEN DOBRE GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
 	</body> 
 </html>
